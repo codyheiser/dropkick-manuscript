fda27b807f6429efe31ee438e39fceceb7632fce
Cody Heiser <codyheiser49@gmail.com>
2020-09-23 19:45:03 -0500

    adjustment to obs label for emptydrops

--- emptydrops_to_h5ad.py
+++ emptydrops_to_h5ad.py
@@ -21,8 +21,8 @@ if __name__ == "__main__":
 
     # add emptydrops results to anndata object in .obs
     a.obs[["EmptyDrops_LogProb","EmptyDrops_pval","EmptyDrops_limited","EmptyDrops_FDR"]] = e[["LogProb","PValue","Limited","FDR"]].values
-    a.obs["EmptyDrops_label"] = "False"
-    a.obs.loc[a.obs.EmptyDrops_FDR <= 0.001, "EmptyDrops_label"] = "True"
+    a.obs["EmptyDrops"] = "False"
+    a.obs.loc[a.obs.EmptyDrops_FDR <= 0.001, "EmptyDrops"] = "True"
 
     # overwrite original .h5ad file
     a.write(args.adata, compression="gzip")
